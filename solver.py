8a31d2ac92abeddb5677d01acdb9bd1fd0c1f8fd
Robert Claus <robertclaus@gmail.com>
2022-04-08 17:09:14 -0500

    Add test

--- solver.py
+++ solver.py
@@ -12,3 +12,17 @@ def best_guess(letter_rules, logging=False):
         log(word_score, possible_words)
 
     return get_best_word(word_score, possible_words)
+
+if __name__ == "__main__":
+    words, words_by_letter = get_words()
+    rules = [
+        {'rule': 'out', 'letter': 'o'}, 
+        {'rule': 'exists', 'letter': 'a'}, 
+        {'rule': 'exists', 'letter': 'r'}, 
+        {'rule': 'exists', 'letter': 's'}, 
+        {'rule': 'notposition', 'letter': 'a', 'position': 1}, 
+        {'rule': 'notposition', 'letter': 'r', 'position': 2}, 
+        {'rule': 'notposition', 'letter': 's', 'position': 4}, 
+        {'rule': 'confirmed', 'letter': 'e', 'position': 5}, 
+    ]
+    assert "scare" in apply_rules(rules)
